f689b7fa843033606240ee55e3901402e6d2e935
Dion Dokter <dev@diondokter.nl>
2026-01-13 10:08:57 +0100

    Change name of log

--- run.ps1
+++ run.ps1
@@ -2,4 +2,4 @@ cargo +stage1 clean
 try {
 Remove-Item .\mir_dump -Recurse -Force -ErrorAction Stop
 } catch {}
-pwsh -Command { $env:RUSTC_LOG="rustc_mir_transform::coroutine=trace"; $env:RUSTFLAGS="-Z dump-mir=coroutine_before|coroutine_resume|coroutine_custom"; cargo +stage1 build --release 2> mir_coroutine_witnesses.log }
+pwsh -Command { $env:RUSTC_LOG="rustc_mir_transform::coroutine=trace"; $env:RUSTFLAGS="-Z dump-mir=coroutine_before|coroutine_resume|coroutine_custom"; cargo +stage1 build --release 2> mir_coroutine.log }
